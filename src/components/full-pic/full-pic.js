8ef735500fe3fca5598807c75e8a8bb0d675b6bb
Arielle Foldoe <afoldoe@gmail.com>
2016-08-24 14:36:44 -0700

    showing full image detail

---
+++ src/components/full-pic/full-pic.js
@@ -0,0 +1,10 @@
+import template from './full-pic.html';
+
+export default {
+  template,
+  bindings: {
+    image: '<'
+  }, 
+  controller: function () {
+  }
+};
---
+++ src/components/images/images.js
@@ -0,0 +1,12 @@
+import template from './images.html';
+
+export default {
+  template,
+  controller: function() {
+    this.images = [{
+      title: 'Dog',
+      url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
+      description: 'Cute dog!'
+    }];
+  }
+};
